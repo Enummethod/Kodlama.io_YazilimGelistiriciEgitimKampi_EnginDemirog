96c5d051875d0a8b1710cde6ca28db6a20d1ad7d
Mustafa Yaşar <80968031+Enummethod@users.noreply.github.com>
2022-11-19 21:54:47 +0300

    Add files via upload

---
+++ Day3/Homework1/YouTubeEgitim/YouTubeEgitim/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeEgitim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+            sayi1 = sayi2;
+            sayi2 = 100;
+
+            int[] sayılar1 = { 1, 2, 3 };
+            int[] sayılar2 = { 10, 20, 3 };
+            sayılar1 = sayılar2;
+            sayılar2[0] = 1000; 
+
+
+            Console.WriteLine(sayi1);
+            Console.WriteLine(sayılar1[0]);
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ Day3/Homework1/YouTubeEgitim2/YouTubeEgitim2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeEgitim2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Ioc Container
+            CustomerManager customerManager = new CustomerManager(new Customer(),new TeacherCreditManager());
+            customerManager.GiveCredit();
+            Console.ReadLine();
+
+        }
+
+
+        class CreditManager
+        {
+            //operasyonları tuttuk.
+            public void Calculate()
+            {
+                Console.WriteLine("Hesaplandı");
+            }
+
+            public void Save()
+            {
+                Console.WriteLine("Kredi verildi.");
+            }     
+        }
+
+        // Katmanlı mimaride temeli "özelliklerin operasyonlardan ayrılması"
+
+        class CustomerManager
+        {
+            private Customer _customer;
+            ICreditManager _creditManager;
+
+            public CustomerManager(Customer customer, ICreditManager creditManager)
+            {
+                _customer = customer;
+                _creditManager = creditManager;
+            }
+            public void Save()
+            {
+                Console.WriteLine(_customer.Id + "nolu müşterisi eklendi.");
+            }
+            public void Delete()
+            {
+                Console.WriteLine(_customer.Id + " müşterisi silindi.");
+            }
+            public void GiveCredit()
+            {
+               _creditManager.Calculate();
+                Console.WriteLine("Kredi verilidi.");
+
+            }
+        }
+
+        class Customer
+        {
+            public Customer()
+            {
+                Console.WriteLine("müşteri nesnesi başlatıldı.");
+            }
+            //Özellik tuttuk.
+            public int Id { get; set; } //property
+            public string City { get; set; }
+        }
+
+        class Company : Customer
+        {
+            public string TaxNumber { get; set; }
+            public string CompanyName { get; set; }
+
+        }
+        class Person: Customer
+        {
+            public string NationalIdentity { get; set; }
+            public string FirsName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        abstract class BaseCreditManager : ICreditManager
+        {
+            public abstract void Calculate();
+          
+            public virtual void Save()
+            {
+                Console.WriteLine("Kaydedildi");
+            }
+        }
+
+        interface ICreditManager
+        {
+            void Calculate();
+            void Save();
+        }
+
+        class TeacherCreditManager : BaseCreditManager, ICreditManager
+        {
+            public override void Calculate()
+            {
+                Console.WriteLine("Öğretmen Kredisi Hesaplandı.");
+            }
+
+            public override void Save()
+            {
+                //
+                base.Save();
+                //
+            }
+
+        }
+        class MilitaryCreditManager : BaseCreditManager, ICreditManager
+        {
+            public override void Calculate()
+            {
+                Console.WriteLine("Asker Kredisi Hesaplandı");
+            }
+     
+        }
+
+    }
+}
